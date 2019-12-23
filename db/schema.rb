c5252f3dae467283a434ca088eed3ef58f5ae9b2
beckhsu <justin7610@gmail.com>
2019-12-24 00:06:00 +0800

    add devise part1

--- db/schema.rb
+++ db/schema.rb
@@ -113,6 +113,18 @@ ActiveRecord::Schema.define(version: 2019_12_27_155844) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "child_teachers", "children"
   add_foreign_key "child_teachers", "teachers"
   add_foreign_key "children", "users"
