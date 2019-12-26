8fed4998b1a737b048ea9859bb8486627c63285a
beckhsu <justin7610@gmail.com>
2019-12-26 22:16:37 +0800

    AddColumsToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_12_27_155844) do
 =======
 ActiveRecord::Schema.define(version: 2019_12_26_100759) do
 >>>>>>> addchildparents
+=======
+ActiveRecord::Schema.define(version: 2019_12_26_140921) do
+>>>>>>> AddColumsToUsers
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +114,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_100759) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "child_id"
 <<<<<<< HEAD
+<<<<<<< HEAD
     t.string "name"
     t.integer "tel"
     t.string "address"
@@ -127,6 +132,11 @@ ActiveRecord::Schema.define(version: 2019_12_26_100759) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
 =======
+=======
+    t.string "name"
+    t.integer "tel"
+    t.string "address"
+>>>>>>> AddColumsToUsers
     t.index ["child_id"], name: "index_users_on_child_id"
 >>>>>>> addchildparents
     t.index ["email"], name: "index_users_on_email", unique: true
