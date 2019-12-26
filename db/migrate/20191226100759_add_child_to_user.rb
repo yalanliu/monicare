5380255d4f1a40ee0fec5979800cdae7be35cdc6
beckhsu <justin7610@gmail.com>
2019-12-26 18:09:14 +0800

    AddChildToUsers

--- db/migrate/20191226100759_add_child_to_user.rb
+++ db/migrate/20191226100759_add_child_to_user.rb
@@ -1,5 +1,9 @@
 class AddChildToUser < ActiveRecord::Migration[6.0]
   def change
+<<<<<<< HEAD
     add_reference :users, :child, foreign_key: true
+=======
+    add_reference :users, :child, null: false, foreign_key: true
+>>>>>>> AddChildToUsers
   end
 end
