9c448c59b4245439079f2e1d6f7e7aa763522cea
Jay <ifiwtbsiwbs@gmail.com>
2019-12-29 16:58:46 +0800

    eat cat not add in database

--- app/controllers/teacher/eat_controller.rb
+++ app/controllers/teacher/eat_controller.rb
@@ -3,6 +3,15 @@ class Teacher::EatController < BabyclassAppliciationController
 
 
   def new
+    @meal_dashboard = Dashboard.new
+  end
+
+  def create
+
+    @meal_dashboard = Dashboard.create(eat_params)
+
+    redirect_to teacher_dashboard_child_path(id: params[:child_id]),notice:'新增成功'
+    
   end
 
   def edit
@@ -13,5 +22,13 @@ class Teacher::EatController < BabyclassAppliciationController
 
   def find_student
     @student = Child.find(params[:child_id])
+  end
+
+  def eat_params
+    params.require(:dashboard).permit(:title, 
+                                      :content, 
+                                      :finished_at,
+                                      :category,
+                                      :child_id)
   end
 end
