1ac9b37d4f1a8dea285fa8b383b7eaf678edb0d2
Jay <ifiwtbsiwbs@gmail.com>
2019-12-31 11:59:48 +0800

    finish medicine/meal frontend and backend

--- app/controllers/teacher/medicine_controller.rb
+++ app/controllers/teacher/medicine_controller.rb
@@ -2,13 +2,14 @@ class Teacher::MedicineController < BabyclassAppliciationController
 
 
   def show 
-
-
     @student = Child.find(params[:child_id])
     @medicine_dashboard = @student.dashboards.medicine.find(params[:id])
   end
 
-
+  private
+  def medicine_params
+    params.require(:dashboard).permit(:admin_sign)
+  end
 
 end
 
---
+++ spec/controllers/medicine_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe MedicineController, type: :controller do
+
+end
