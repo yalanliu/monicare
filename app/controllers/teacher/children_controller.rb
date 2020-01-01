b6b68bee93604c487c75db40b104e28bd8628f5d
yalan <lanya4190@gmail.com>
2020-01-01 21:30:47 +0800

    add backend pick_up view

--- app/controllers/teacher/children_controller.rb
+++ app/controllers/teacher/children_controller.rb
@@ -11,6 +11,7 @@ class Teacher::ChildrenController < BabyclassAppliciationController
   def show
     @student = Child.find(params[:id])
     @dashboards = @student.dashboards
+    @pick_ups = @student.pick_ups
   end
 
 end
