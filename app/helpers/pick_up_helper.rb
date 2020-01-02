ab76d6d9aa94d411fb6d025a12f2d27711c1b5ed
yalan <lanya4190@gmail.com>
2020-01-02 11:18:06 +0800

    build preview pic function

---
+++ app/helpers/pick_up_helper.rb
@@ -0,0 +1,21 @@
+module PickUpHelper
+  def  cover_image_tag(pick_up)
+    if pick_up.pick_up_pic?
+      image_tag pick_up.pick_up_pic.thumb.url, id: 'preview_pick_up_pic'
+    else
+      image_tag fake_image, id: 'preview_pick_up_pic'
+    end
+  end
+
+  def show_image(pick_up)
+    if pick_up.pick_up_pic?
+      pick_up.pick_up_pic
+    else
+      fake_image
+    end
+  end
+  private
+  def fake_image
+    "https://fakeimg.pl/300x300/FFDD55/FFA488/?text=請上傳圖&font=noto"
+  end
+end
--- app/javascript/packs/pick_up.js
+++ app/javascript/packs/pick_up.js
@@ -10,3 +10,20 @@ window.onload = function() {
   document.location.href.match(anchor)[0];
   document.querySelector('#list-pick-up-list').click();
 };
+
+function readURL(uploadPicInput) {
+  if (uploadPicInput.files && uploadPicInput.files[0]) {
+    let reader = new FileReader();
+    
+    reader.onload = function(picUrl) {
+      $('#preview_pick_up_pic').attr('src', picUrl.target.result);
+    }
+    
+    reader.readAsDataURL(uploadPicInput.files[0]);
+    $('.pick_up_pic_url').text(uploadPicInput.files[0].name);
+  }
+}
+
+$("#pick_up_pick_up_pic").change(function() {
+  readURL(this);
+});
