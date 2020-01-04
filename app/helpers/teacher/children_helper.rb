efee7f468647209151cee6db8310b58ab5c76b1f
Jay <ifiwtbsiwbs@gmail.com>
2020-01-04 14:23:31 +0800

    finish teacher/medicine admin_sign and overview helper

---
+++ app/helpers/teacher/children_helper.rb
@@ -0,0 +1,9 @@
+module Teacher::ChildrenHelper
+  def teacher_redirect_to(dashboard)
+    if dashboard.category != 'medicine' 
+      "#{dashboard.category}/#{dashboard.id}/edit"
+     else
+      "#{dashboard.category}/#{dashboard.id}"
+     end
+  end
+end
