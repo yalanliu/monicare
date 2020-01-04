c185afbca7b0ee954a6f4a147afd6a66ab4cf1dd
Jay <ifiwtbsiwbs@gmail.com>
2020-01-04 14:23:31 +0800

    finish teacher/medicine admin_sign and overview helper

--- app/controllers/teacher/medicine_controller.rb
+++ app/controllers/teacher/medicine_controller.rb
@@ -1,10 +1,23 @@
 class Teacher::MedicineController < BabyclassAppliciationController
 
-  def show 
+  def show
+    @student = Child.find(params[:child_id])
+    @medicine_dashboard = @student.dashboards.medicine.find(params[:id])
+  end
+  def edit 
     @student = Child.find(params[:child_id])
     @medicine_dashboard = @student.dashboards.medicine.find(params[:id])
   end
 
+  def update
+    @student = Child.find(params[:child_id])
+    @medicine_dashboard = @student.dashboards.medicine.find(params[:id])
+    if @medicine_dashboard.update(medicine_params)
+      redirect_to teacher_dashboard_child_medicine_path, notice:'更新成功'
+    else
+      render :edit, notice:'更新失敗，餵藥品者請簽名'
+    end
+  end
   private
   def medicine_params
     params.require(:dashboard).permit(:admin_sign)
--- app/helpers/children_helper.rb
+++ app/helpers/children_helper.rb
@@ -1,2 +1,9 @@
 module ChildrenHelper
+  def parent_redirect_to(dashboard)
+    if dashboard.category == 'medicine'
+      "/dashboard/children/#{dashboard.child_id}/#{dashboard.category}/#{dashboard.id}/edit"
+    else
+     "/dashboard/children/#{dashboard.child_id}/#{dashboard.category}/#{dashboard.id}"
+    end
+  end
 end
