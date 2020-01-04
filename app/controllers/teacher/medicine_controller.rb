45ac4e21fe5232328960e6434c78a203c5169a91
Jay <ifiwtbsiwbs@gmail.com>
2020-01-04 15:42:03 +0800

    add medicine controller before action

--- app/controllers/teacher/medicine_controller.rb
+++ app/controllers/teacher/medicine_controller.rb
@@ -1,17 +1,13 @@
 class Teacher::MedicineController < BabyclassAppliciationController
+  before_action :find_student, only:[:show, :edit, :update]
+  before_action :find_dashboard, only:[:show, :edit, :update]
 
   def show
-    @student = Child.find(params[:child_id])
-    @medicine_dashboard = @student.dashboards.medicine.find(params[:id])
   end
-  def edit 
-    @student = Child.find(params[:child_id])
-    @medicine_dashboard = @student.dashboards.medicine.find(params[:id])
+  def edit
   end
 
   def update
-    @student = Child.find(params[:child_id])
-    @medicine_dashboard = @student.dashboards.medicine.find(params[:id])
     if @medicine_dashboard.update(medicine_params)
       redirect_to teacher_dashboard_child_medicine_path, notice:'更新成功'
     else
@@ -21,6 +17,14 @@ class Teacher::MedicineController < BabyclassAppliciationController
   private
   def medicine_params
     params.require(:dashboard).permit(:admin_sign)
+  end
+
+  def find_student
+    @student = Child.find(params[:child_id])
+  end
+
+  def find_dashboard
+    @medicine_dashboard = @student.dashboards.medicine.find(params[:id])
   end
 end
 
