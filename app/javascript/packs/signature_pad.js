2c1b180360fca72dab4eb0a435c0ee3d25b158a7
yalan <lanya4190@gmail.com>
2020-01-05 21:23:07 +0800

    finish admin_sign

--- app/javascript/packs/signature_pad.js
+++ app/javascript/packs/signature_pad.js
@@ -5,6 +5,9 @@ let canvas = document.getElementById('signature-pad');
 
 window.onload = function() {
   signaturePad.off();
+  $('.signature-pad-wrapper').hide();
+  $('#clear').hide();
+  $('#save').hide();
 }
 
 function resizeCanvas() {
@@ -18,25 +21,15 @@ window.onresize = resizeCanvas;
 resizeCanvas();
 
 let signaturePad = new SignaturePad(canvas, {
-  backgroundColor: 'rgb(255, 255, 255)' // necessary for saving image as JPEG; can be removed is only saving as PNG or SVG
-});
-
-document.getElementById('save').addEventListener('click', function () {
-  if (signaturePad.isEmpty()) {
-    return alert("請務必簽名!");
-  }
-
-  let data = signaturePad.toDataURL();
-  console.log(data);
-  signaturePad.off();
-  $('#dashboard_parent_sign').attr('value', data);
+  backgroundColor: 'rgb(255, 255, 255)'
 });
 
 document.getElementById('draw').addEventListener('click', function () {
+  $('.signature-pad-wrapper').show();
   signaturePad.on();
-  let ctx = canvas.getContext('2d');
-  console.log(ctx.globalCompositeOperation);
-  ctx.globalCompositeOperation = 'source-over';
+  $('#draw').hide();
+  $('#clear').show();
+  $('#save').show();
 });
 
 document.getElementById('clear').addEventListener('click', function () {
@@ -44,10 +37,13 @@ document.getElementById('clear').addEventListener('click', function () {
   signaturePad.on();
 });
 
-document.getElementById('undo').addEventListener('click', function () {
-	let data = signaturePad.toData();
-  if (data) {
-    data.pop(); // remove the last dot or line
-    signaturePad.fromData(data);
+document.getElementById('save').addEventListener('click', function () {
+  if (signaturePad.isEmpty()) {
+    return alert("請務必簽名!");
   }
+  let data = signaturePad.toDataURL();
+  signaturePad.off();
+  $('#dashboard_parent_sign').attr('value', data);
+  $('#dashboard_admin_sign').attr('value', data);
+  alert("已簽署完成");
 });
