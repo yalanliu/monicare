5b43dd8088e36fe1fbca42f90b8585afe8f58197
yalan <lanya4190@gmail.com>
2020-01-10 11:24:02 +0800

    design homepage + dashboard + overview

--- app/controllers/babyclass_appliciation_controller.rb
+++ app/controllers/babyclass_appliciation_controller.rb
@@ -2,6 +2,8 @@ class BabyclassAppliciationController < ApplicationController
   before_action :authenticate_teacher!
   before_action :baby_class
   
+  layout 'backend'
+
   private
   def baby_class
     @babyclass = current_teacher.babyclasses
